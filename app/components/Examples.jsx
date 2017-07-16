56289a519326735d9be6aee1a59fecae083115af
telco2011 <davidlopez.david@gmail.com>
2017-07-16 16:13:31 +1000

    Modified styles

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return (
     <div>
-      <h3>Examples</h3>
-      <p>Welcome to examples page!</p>
+      <h1 className="text-center">Examples</h1>
+      <p>Here are a few example locations to try out!</p>
+      <ul>
+        <li>
+          <Link to='/?location=Brisbane,au'>Brisbane, AU</Link>
+        </li>
+        <li>
+          <Link to='/?location=Barcelona,es'>Barcelona, ES</Link>
+        </li>
+      </ul>
     </div>
   );
 };
