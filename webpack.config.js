780897bddbbed30d91352c874cba9273aee9909b
kratos <davidlopez.david@gmail.com>
2017-07-17 17:27:00 +1000

    Added sass

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var webpack = require('webpack');
 module.exports = {
     entry: [
         'script!jquery/dist/jquery.min.js',
-        'script!foundation-sites/dist/js/foundation.min.js',
+        'script!foundation-sites/dist/foundation.min.js',
         './app/app.jsx'
     ],
     externals: {
@@ -31,7 +31,7 @@ module.exports = {
             Examples: 'app/components/Examples.jsx',
             openWeatherMap: 'app/api/openWeatherMap.jsx',
             ErrorModal: 'app/components/ErrorModal.jsx',
-            applicationStyles: 'app/styles/app.css'
+            applicationStyles: 'app/styles/app.scss'
         },
         extensions: ['', '.js', '.jsx']
     },
